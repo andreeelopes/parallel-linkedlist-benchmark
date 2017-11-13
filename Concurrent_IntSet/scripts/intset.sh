5b32a321f7201defc422517dc2f009b7e6eacb9a
Coquenim <nelson_coquenim@hotmail.com>
2017-11-13 18:25:42 +0000

    Major changes to test script

--- Concurrent_IntSet/scripts/intset.sh
+++ Concurrent_IntSet/scripts/intset.sh
@@ -1,21 +1,12 @@
-#!/bin/sh
-
+#!/bin/sh 
 java="java -enableassertions -verbose:gc -Xms1024M"
 
-warmup=2000
+warmup=1
 duration=${1}
 nr_threads=${2:-1}
 write_perc=${3:-50}
+list_impl=${4}
 
-
-#list_impl=LinkedList
-# list_impl=Synchronized
-# list_impl=GlobalLock
-# list_impl=GlobalRWLock
-# list_impl=PerNodeLock
-# list_impl=OptimisticPerNodeLock
-# list_impl=LazyPerNodeLock
- list_impl=LockFree
 value_range=262144
 initial_size=256
 
@@ -23,4 +14,3 @@ initial_size=256
 ${java} -cp bin cp.benchmark.Driver -d ${duration} -w ${warmup} -n ${nr_threads} \
                 cp.benchmark.intset.Benchmark ${list_impl} \
                 -r ${value_range} -i ${initial_size} -w ${write_perc}
-                
--- Concurrent_IntSet/scripts/testscript.sh
+++ Concurrent_IntSet/scripts/testscript.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-rm results.txt
 
-for r in 50 10
+## all lock implementations
+declare -a arr=("Synchronized" "GlobalLock" "GlobalRWLock" "PerNodeLock" "OptimisticPerNodeLock" "LazyPerNodeLock" "LockFree")
+
+for i in "${arr[@]}"
 do
-    for n in 1 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32
-    do
-        for i in 1 2 3 4 5 6 7 8 9 10
-        do
-        	bash ./scripts/intset.sh 10 $n $r >> results.txt        	
-        done
-        echo "" >> results.txt
-    done
+	rm "results_${arr[i]}.txt"
+	echo "$i"
+	#percentage of writes
+	for r in 50 10
+	do
+		#number of threads
+	    for n in 1 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32
+	    do
+	    	#number of tries
+	        for j in 1 2 3 4 5
+	        do
+	        	bash ./scripts/intset.sh 10 $n $r $i >> "results_${arr[i]}.txt"       	
+	        done
+	        echo "" >> "results_${arr[i]}.txt"
+	    done
+	done
 done
 echo "Test concluded."
