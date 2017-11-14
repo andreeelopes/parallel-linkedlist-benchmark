d7602613874273aa163aba7b4787d4924ead5fc7
Coquenim <nelson_coquenim@hotmail.com>
2017-11-14 09:35:33 +0000

    Added new script for varying read percentage

--- Concurrent_IntSet/scripts/testscript_reads.sh
+++ Concurrent_IntSet/scripts/testscript_reads.sh
@@ -7,27 +7,6 @@ for i in "${arr[@]}"
 do
 	echo "$i"
 	#percentage of writes
-	for r in 50 10
-	do
-		#number of threads
-	    for n in 1 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32
-	    do
-	    	#number of tries
-	        for j in 1 2 3 4 5
-	        do
-	        	bash ./scripts/intset.sh 1000 $n $r $i >> "results_$i.txt"       	
-	        done
-	        echo "" >> "results_$i.txt"
-	    done
-	done
-
-done
-echo "Test concluded."
-
-for i in "${arr[@]}"
-do
-	echo "$i"
-	#percentage of writes
 	for r in 0 10 20 30 40 50 60 70 80 90
 	do
 		#number of tries
