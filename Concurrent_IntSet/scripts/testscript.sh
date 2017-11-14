1b6c05427e74dae3d9431f757591d3d63a45ac11
Coquenim <nelson_coquenim@hotmail.com>
2017-11-14 09:37:13 +0000

    Minor tweak to test file

--- Concurrent_IntSet/scripts/testscript.sh
+++ Concurrent_IntSet/scripts/testscript.sh
@@ -24,19 +24,3 @@ do
 
 done
 echo "Test concluded."
-
-for i in "${arr[@]}"
-do
-	echo "$i"
-	#percentage of writes
-	for r in 0 10 20 30 40 50 60 70 80 90
-	do
-		#number of tries
-        for j in 1 2 3 4 5
-        do
-        	bash ./scripts/intset.sh 1000 32 $r $i >> "results_reads_$i.txt"       	
-        done
-        echo "" >> "results_reads_$i.txt"
-	done
-done
-echo "Test concluded."
