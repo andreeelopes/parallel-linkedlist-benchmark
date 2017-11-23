650d96837f28acb67db828b76d8e1ee768235e9b
Coquenim <nelson_coquenim@hotmail.com>
2017-11-23 14:50:05 +0000

    Changed plot script to accommodate both list sizes results

--- Concurrent_IntSet/scripts/graph.py
+++ Concurrent_IntSet/scripts/graph.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
 
 def split_contains_ratio(M, n_split):
     M_mean = np.mean(M, axis = 1)
@@ -24,41 +26,59 @@ def plot_contains_ratio(Sync, GL, RWL, PNL, OL, Lazy, Free, X, title, xlabel, ti
     plt.savefig(titleToSave + ".png")
     plt.show()
     plt.close()
+    
+#LOAD DATA FROM FILES
 
+#Larger List
+matSync_bigger = np.loadtxt('../results/bigger_Synchronized.txt') * 1000
+matGL_bigger = np.loadtxt('../results/bigger_GlobalLock.txt') * 1000
+matRWL_bigger = np.loadtxt('../results/bigger_GlobalRWLock.txt') * 1000
+matPNL_bigger = np.loadtxt('../results/bigger_PerNodeLock.txt') * 1000
+matOL_bigger = np.loadtxt('../results/bigger_OptimisticPerNodeLock.txt') * 1000
+matLazy_bigger = np.loadtxt('../results/bigger_LazyPerNodeLock.txt') * 1000
+matFree_bigger = np.loadtxt('../results/bigger_LockFree.txt') * 1000
+#Smaller List
+matSync_smaller = np.loadtxt('../results/results_Synchronized.txt') * 1000
+matGL_smaller = np.loadtxt('../results/results_GlobalLock.txt') * 1000
+matRWL_smaller = np.loadtxt('../results/results_GlobalRWLock.txt') * 1000
+matPNL_smaller = np.loadtxt('../results/results_PerNodeLock.txt') * 1000
+matOL_smaller = np.loadtxt('../results/results_OptimisticPerNodeLock.txt') * 1000
+matLazy_smaller = np.loadtxt('../results/results_LazyPerNodeLock.txt') * 1000
+matFree_smaller = np.loadtxt('../results/results_LockFree.txt') * 1000
 
-#PLOT HIGH AND LOW CONTAINS RATIO
-
-matSync = np.loadtxt('../results/results_Synchronized.txt') * 1000
-matGL = np.loadtxt('../results/results_GlobalLock.txt') * 1000
-matRWL = np.loadtxt('../results/results_GlobalRWLock.txt') * 1000
-matPNL = np.loadtxt('../results/results_PerNodeLock.txt') * 1000
-matOL = np.loadtxt('../results/results_OptimisticPerNodeLock.txt') * 1000
-matLazy = np.loadtxt('../results/results_LazyPerNodeLock.txt') * 1000
-matFree = np.loadtxt('../results/results_LockFree.txt') * 1000
-
-
-contains_Sync = split_contains_ratio(matSync,4)
-contains_GL = split_contains_ratio(matGL,4)
-contains_RWL = split_contains_ratio(matRWL,4)
-contains_PNL = split_contains_ratio(matPNL,4)
-contains_OL = split_contains_ratio(matOL,4)
-contains_Lazy = split_contains_ratio(matLazy,4)
-contains_Free = split_contains_ratio(matFree,4)
+#Parametres
+n_reads = [10, 90, 50];
 threads = [1,2,4,6,8,10,12,14,16]
 
+#MAKE AVERAGES AND SPLITS RESULTS
 
-plot_contains_ratio( contains_Sync[3], contains_GL[3], contains_RWL[3], contains_PNL[3], contains_OL[3], 
-     contains_Lazy[3], contains_Free[3],threads, "Ops/sec(99% Reads)", "Threads", "../results/Ops_sec_99_reads")
+#Larger List
+contains_Sync_bigger = split_contains_ratio(matSync_bigger,len(n_reads))
+contains_GL_bigger   = split_contains_ratio(matGL_bigger,len(n_reads))
+contains_RWL_bigger  = split_contains_ratio(matRWL_bigger,len(n_reads))
+contains_PNL_bigger  = split_contains_ratio(matPNL_bigger,len(n_reads))
+contains_OL_bigger   = split_contains_ratio(matOL_bigger,len(n_reads))
+contains_Lazy_bigger = split_contains_ratio(matLazy_bigger,len(n_reads))
+contains_Free_bigger = split_contains_ratio(matFree_bigger,len(n_reads))
+#Smaller List
+contains_Sync_smaller = split_contains_ratio(matSync_smaller,len(n_reads)+1)
+contains_GL_smaller   = split_contains_ratio(matGL_smaller,len(n_reads)+1)
+contains_RWL_smaller  = split_contains_ratio(matRWL_smaller,len(n_reads)+1)
+contains_PNL_smaller  = split_contains_ratio(matPNL_smaller,len(n_reads)+1)
+contains_OL_smaller   = split_contains_ratio(matOL_smaller,len(n_reads)+1)
+contains_Lazy_smaller = split_contains_ratio(matLazy_smaller,len(n_reads)+1)
+contains_Free_smaller = split_contains_ratio(matFree_smaller,len(n_reads)+1)
 
-plot_contains_ratio( contains_Sync[2], contains_GL[2], contains_RWL[2], contains_PNL[2], contains_OL[2], 
-     contains_Lazy[2], contains_Free[2],threads, "Ops/sec(10% Reads)", "Threads", "../results/Ops_sec_10_reads")
 
-plot_contains_ratio( contains_Sync[1], contains_GL[1], contains_RWL[1], contains_PNL[1], contains_OL[1], 
-     contains_Lazy[1], contains_Free[1], threads, "Ops/sec(90% Reads)", "Threads", "../results/Ops_sec_90_reads")
-
-plot_contains_ratio( contains_Sync[0], contains_GL[0], contains_RWL[0], contains_PNL[0], contains_OL[0], 
-     contains_Lazy[0], contains_Free[0],threads, "Ops/sec(50% Reads)", "Threads", "../results/Ops_sec_50_reads")
-
+#PLOT RESULTS
+#Larger List
+for i in range(0,len(n_reads)):
+    plot_contains_ratio( contains_Sync_bigger[i], contains_GL_bigger[i], contains_RWL_bigger[i], contains_PNL_bigger[i], contains_OL_bigger[i], 
+     contains_Lazy_bigger[i], contains_Free_bigger[i],threads, "Ops/sec(" + str(n_reads[i]) + "% Reads)", "Threads", "../results/4000_" + str(n_reads[i]) +"_reads")
+#Smaller list
+for i in range(0,len(n_reads)+1):
+    plot_contains_ratio( contains_Sync_smaller[i], contains_GL_smaller[i], contains_RWL_smaller[i], contains_PNL_smaller[i], contains_OL_smaller[i], 
+     contains_Lazy_smaller[i], contains_Free_smaller[i],threads, "Ops/sec(" + str(n_reads[i]) + "% Reads)", "Threads", "../results/4000_" + str(n_reads[i]) +"_reads")
     
 #PLOT AS CONTAINS INCREASES
 
@@ -81,7 +101,7 @@ contains_Free = split_contains_ratio(matFree,11)
 
 contains_perc = [0,5,10,20,30,40,50,60,70,80,90]
 plot_contains_ratio(contains_Sync, contains_GL, contains_RWL, contains_PNL, contains_OL, 
-     contains_Lazy, contains_Free, contains_perc, "Ops/sec(16 Threads)", "% contains", "../results/Ops_sec_16_threads")
+     contains_Lazy, contains_Free, contains_perc, "Ops/sec(16 Threads)", "% writes", "../results/Ops_sec_16_threads")
 
 
 
